2f6647cd8597b271f8c1476d55640faf3a1d432a
mmtech255 <139677096+mmtech255@users.noreply.github.com>
2025-07-24 23:38:57 +0530

    Update Hotstar.php

--- Hotstar.php
+++ Hotstar.php
@@ -1,1 +1,2 @@
 
+https://jiotvmblive.cdn.jio.com/bpk-tv/Disney_Channel_BTS/output/index.mpd?__hdnea__=st=1753356221~exp=1753442621~acl=/*~hmac=187657618f6001a248ca2b8f62736b66d3ac1ad8af5666930973d83405912b58|inputstream.adaptive.manifest_type=mpd&inputstream.adaptive.license_type=clearkey&inputstream.adaptive.license_key=5181d3e6698055578cedc5bfc86b3e56:3dca7917d8cf9bb7095dc72b48bdcd3c
