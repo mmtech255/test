0bfe4e85e8c0372200fc17df195c786d90895352
mmtech255 <139677096+mmtech255@users.noreply.github.com>
2025-07-24 23:42:26 +0530

    Update Hotstar.php

--- Hotstar.php
+++ Hotstar.php
@@ -1,4 +1,6 @@
-#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#EXTINF:-1 tvg-id="DisneyChannel" tvg-logo="https://jiotvimages.cdn.jio.com/dare_images/images/Disney_Channel.png" group-title="Kids",Disney Channel
 #KODIPROP:inputstream.adaptive.license_type=clearkey
 #KODIPROP:inputstream.adaptive.license_key=5181d3e6698055578cedc5bfc86b3e56:3dca7917d8cf9bb7095dc72b48bdcd3c
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36
 https://jiotvmblive.cdn.jio.com/bpk-tv/Disney_Channel_BTS/output/index.mpd?__hdnea__=st=1753356221~exp=1753442621~acl=/*~hmac=187657618f6001a248ca2b8f62736b66d3ac1ad8af5666930973d83405912b58
