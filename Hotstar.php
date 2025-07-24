2a0cb546ff2353a780f3fab70c8128d9e1b2e7f9
mmtech255 <139677096+mmtech255@users.noreply.github.com>
2025-07-24 21:22:39 +0530

    Update Hotstar.php

--- Hotstar.php
+++ Hotstar.php
@@ -1,6 +1,11 @@
-#EXTM3U
-#EXTINF:-1,Playlist 1
-https://raw.githubusercontent.com/mmtech255/test/refs/heads/main/Tataplay.php
-
-#EXTINF:-1,Playlist 2
-https://sflex07.fun/SFJSTARM3U/JTVPLUS1717.php
+<!DOCTYPE html>
+<html>
+<head><title>My Playlists</title></head>
+<body>
+  <h2>Available Playlists</h2>
+  <ul>
+    <li><a href="https://raw.githubusercontent.com/mmtech255/test/refs/heads/main/Tataplay.php" target="_blank">Playlist 1</a></li>
+    <li><a href="https://sflex07.fun/SFJSTARM3U/JTVPLUS1717.php" target="_blank">Playlist 2</a></li>
+  </ul>
+</body>
+</html>
